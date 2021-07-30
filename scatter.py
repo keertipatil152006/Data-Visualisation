3f0fd3323fbebe4ee74c2933e6f1690aa96419a2
keertipatil152006 <73159617+keertipatil152006@users.noreply.github.com>
2021-07-30 20:54:21 +0530

    Update scatter.py

--- scatter.py
+++ scatter.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import plotly.express as px
 
-df=pd.read_csv('data.csv')
-fig=px.scatter(df,x='Date',y='Cases',color='Country',size='Percentage',title='Corona Cases')
-fig.show()
+df = pd.read_csv("data.csv")
 
+fig = px.scatter(df,    x="date",
+                        y="cases",
+                        color="country"
+            )
+fig.show()
 
